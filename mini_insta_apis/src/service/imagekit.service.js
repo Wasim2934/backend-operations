f5064752f95df4bd96cdab5a5f96cf4da0282212
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-09-07 22:56:34 +0530

    imageKit services

--- mini_insta_apis/src/service/imagekit.service.js
+++ mini_insta_apis/src/service/imagekit.service.js
@@ -2,7 +2,7 @@ import imagekit from "imagekit";
 import dotenv from "dotenv";
 dotenv.config();
 
-const imagekitInstance = new imagekit({
+const imagekitInstance = new imagekit({    
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
